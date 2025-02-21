b42f23506c092ee94e32507f5c4e657d7dd4f7e4
Eduarda Alves <alvesneivam1203@gmail.com>
2025-02-21 11:44:58 -0300

    Updatel

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// Seleciona todos os inputs de rádio
+const ratings = document.querySelectorAll('input[name="rating"]');
+const resultDiv = document.getElementById('result');
+
+// Adiciona um evento de mudança a cada input de rádio
+ratings.forEach((rating) => {
+    rating.addEventListener('change', () => {
+        const selectedValue = rating.value;
+        resultDiv.textContent = `Você avaliou o site com ${selectedValue} estrela(s)!`;
+    });
+});
