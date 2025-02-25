5f746b62b6049d483af130312dc9720b42986148
Eduarda Alves <alvesneivam1203@gmail.com>
2025-02-25 11:34:58 -0300

    Update v6

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
-/*!
-* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
 
-// Seleciona todos os inputs de rádio
 const ratings = document.querySelectorAll('input[name="rating"]');
 const resultDiv = document.getElementById('result');
 
